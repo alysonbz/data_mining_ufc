e9abafc2666d20875b6fd02e3a8f91bb07b22484
lauramendescd <lauramendescd@gmail.com>
2023-09-05 15:53:50 -0300

    atualização

--- unidade1-clusters/2_hierarchical_clustering/1_ward-method.py
+++ unidade1-clusters/2_hierarchical_clustering/1_ward-method.py
@@ -1,17 +1,17 @@
 from src.utils import load_comic_con_dataset
-from scipy.cluster.hierarchy import ____, ____
+from scipy.cluster.hierarchy import linkage, fcluster
+import seaborn as sns
+import matplotlib.pyplot as plt
 
+# função load_comic_con_dataset
 comic_con = load_comic_con_dataset()
 
-# Import the fcluster and linkage functions
+# função linkage() para calcular a matriz de ligação
+distance_matrix = linkage(comic_con[['x_scaled', 'y_scaled']], method='ward', metric='euclidean')
 
-# Use the linkage() function
-distance_matrix = ____(comic_con[['x_scaled', 'y_scaled']], ____ = ____, metric = 'euclidean')
+# rótulos de cluster usando a função fcluster com base em um critério de número máximo de clusters
+comic_con['cluster_labels'] = fcluster(distance_matrix, t=2, criterion='maxclust')
 
-# Assign cluster labels
-comic_con['cluster_labels'] = ____(____, ____, criterion='maxclust')
-
-# Plot clusters
-sns.scatterplot(x='x_scaled', y='y_scaled',
-                hue='cluster_labels', data = comic_con)
+# clusters
+sns.scatterplot(x='x_scaled', y='y_scaled', hue='cluster_labels', data=comic_con)
 plt.show()
