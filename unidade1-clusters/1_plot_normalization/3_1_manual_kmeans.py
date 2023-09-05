9f78f4cb87a4dbcca9ec87acfd7b734dc7cb0a64
mateus <mateussilvamatos@alu.ufc.br>
2023-09-05 09:59:26 -0300

    Merge branch 'main' of https://github.com/alysonbz/data_minning_ufc into mateus_silva
    # Conflicts:
    #	unidade1/cluster_analisys.py

--- unidade1-clusters/1_plot_normalization/3_1_manual_kmeans.py
+++ unidade1-clusters/1_plot_normalization/3_1_manual_kmeans.py
@@ -1,37 +1,81 @@
+import random
+import numpy as np
 from src.utils import load_pokemon_dataset
-
-
+import pandas as pd
+import matplotlib.pyplot as plt
 df = load_pokemon_dataset()
+x_values, y_values = load_pokemon_dataset()
 
+df = pd.DataFrame({'x': x_values, 'y': y_values})
 
-def set_random_cluster_coordinate(num_of_cluster):
-    coord_list = []
-    label_list = range(0,num_of_cluster)
-    #preecher a lista com quatro coordenadas aleatótias.
-    return coord_list , label_list
+class KMeans:
+    def __init__(self,n_clusters=2,max_iter=100):
+        self.n_clusters = n_clusters
+        self.max_iter = max_iter
+        self.centroids = None
 
-def create_points(df):
-    coords = [] # [ [x1,y1], [x2,y2] , [x3,y3]....]
-    return coords
+    def fit_predict(self,X):
 
-def dist_euclidian(p1,p2):
-    dist = 0
-    return dist
+        random_index = random.sample(range(0,X.shape[0]),self.n_clusters)
+        self.centroids = X[random_index]
 
-def kmeans(df,num_of_cluster):
+        for i in range(self.max_iter):
+            # assign clusters
+            cluster_group = self.assign_clusters(X)
+            old_centroids = self.centroids
+            # move centroids
+            self.centroids = self.move_centroids(X,cluster_group)
+            # check finish
+            if (old_centroids == self.centroids).all():
+                break
 
-    centroids ,  centroids_labels = set_random_cluster_coordinate(num_of_cluster)
-    increase_cluster = True
-    coords  = create_points(df)
-    coord_label = []  #
-    while increase_cluster == True:
-        for coord in coords:
-           coord_label.append(None)
-            ##algoritmo
+        return cluster_group
 
-    return  coord_label
+    def assign_clusters(self,X):
+        cluster_group = []
+        distances = []
 
+        for row in X:
+            for centroid in self.centroids:
+                distances.append(np.sqrt(np.dot(row-centroid,row-centroid)))
+            min_distance = min(distances)
+            index_pos = distances.index(min_distance)
+            cluster_group.append(index_pos)
+            distances.clear()
 
+        return np.array(cluster_group)
 
+    def move_centroids(self,X,cluster_group):
+        new_centroids = []
 
-num_of_clusters = 2
+        cluster_type = np.unique(cluster_group)
+
+        for type in cluster_type:
+            new_centroids.append(X[cluster_group == type].mean(axis=0))
+
+        return np.array(new_centroids)
+num_clusters = 2
+kmeans = KMeans(n_clusters=num_clusters)
+
+X = df[['x', 'y']].values
+cluster_labels = kmeans.fit_predict(X)
+df['cluster'] = cluster_labels
+
+clustered_data = df[['x', 'y', 'cluster']]
+# Separar os dados por cluster
+cluster_data = [clustered_data[clustered_data['cluster'] == i] for i in range(num_clusters)]
+colors = ['b', 'g', 'r', 'c', 'm', 'y', 'k']
+plt.figure(figsize=(8, 6))
+for i in range(num_clusters):
+    plt.scatter(cluster_data[i]['x'], cluster_data[i]['y'], label=f'Cluster {i}', c=colors[i])
+
+# Plotar os centroides
+centroids = kmeans.centroids
+for i, centroid in enumerate(centroids):
+    plt.scatter(centroid[0], centroid[1], marker='X', s=200, c='black', label=f'Centroide {i}')
+
+plt.xlabel('X')
+plt.ylabel('Y')
+plt.title('K-Means')
+plt.legend()
+plt.show()
