488a10d95f06dcc8352ce85600b8fc5ab73949a4
lauramendescd <lauramendescd@gmail.com>
2023-09-05 15:36:04 -0300

    Revert "Update 1_cluster_plot.py"
    This reverts commit ae370d6978aecdedd9cb59a56b616944d63ff458.

--- unidade1-clusters/1_plot_normalization/1_cluster_plot.py
+++ unidade1-clusters/1_plot_normalization/1_cluster_plot.py
@@ -3,10 +3,10 @@ from src.utils import load_pokemon_dataset
 x,y = load_pokemon_dataset()
 
 # Import plotting class from matplotlib library
-from matplotlib import pyplot as plt
+from matplotlib import ____ as plt
 
 # Create a scatter plot
-plt.scatter(x, y)
+plt.____(x, y)
 
 # Display the scatter plot
-plt.show()
+plt.____()
