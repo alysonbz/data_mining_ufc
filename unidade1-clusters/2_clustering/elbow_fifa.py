cef324f4e85c09256b9fab09b419676dc58ee000
lauramendescd <lauramendescd@gmail.com>
2023-09-15 12:36:22 -0300

    atualização

---
+++ unidade1-clusters/2_clustering/elbow_fifa.py
@@ -0,0 +1,25 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+
+from scipy.cluster.vq import kmeans
+from src.utils import load_fifa_dataset
+
+fifa_18 = load_fifa_dataset()
+
+distortions = []
+num_clusters = range(1, 7)
+
+# Create a list of distortions from the kmeans function
+for i in num_clusters:
+    cluster_centers, distortion = kmeans(fifa_18[['sliding_tackle','aggression']], i)
+# append distortion on list distortions
+    distortions.append(distortion)
+
+# Create a DataFrame with two lists - num_clusters, distortions
+elbow_plot = pd.DataFrame({'num_clusters': num_clusters, 'distortions': distortions})
+
+# Creat a line plot of num_clusters and distortions
+sns.lineplot(x='num_clusters', y='distortions', data = elbow_plot)
+plt.xticks(num_clusters)
+plt.show()
