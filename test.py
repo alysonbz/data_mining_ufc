b10d4c9af6ae28906defb51bac1e974fbe40fbd5
mateus <mateussilvamatos@alu.ufc.br>
2023-09-19 16:48:58 -0300

    Merge branch 'main' of https://github.com/alysonbz/data_minning_ufc into mateus_silva
    # Conflicts:
    #	unidade1/cluster_analisys.py

---
+++ test.py
@@ -0,0 +1,11 @@
+import re
+'''resp = re.match('mateus','mateus eh lindo')
+print(resp)
+resp1 = re.match('feio','mateus eh feio')
+print(resp1)
+word_regex = '\s+'
+resp = re.split(word_regex,'semana de aula')
+print(resp)
+word_regex = r'[a-z]\w+'
+resp = re.split(word_regex,'4Semana Quente! De Aula')
+print(resp)'''
