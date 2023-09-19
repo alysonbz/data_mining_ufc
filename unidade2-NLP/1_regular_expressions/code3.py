5e25b9f1ef3ad6a48c8fc82dc2d7095e0d1c38da
lauramendescd <lauramendescd@gmail.com>
2023-09-19 18:31:51 -0300

    atualização

--- unidade2-NLP/1_regular_expressions/code3.py
+++ unidade2-NLP/1_regular_expressions/code3.py
@@ -1,21 +1,34 @@
 from nltk.tokenize import sent_tokenize
-from NLP.src.nlp_utils import get_sample_Santo_Graal
-
-scene_one =  get_sample_Santo_Graal()
-sentences =  sent_tokenize(scene_one)
+from src.utils import get_sample_Santo_Graal
+import re
+scene_one = get_sample_Santo_Graal()
+sentences = sent_tokenize(scene_one)
 # Search for the first occurrence of "coconuts" in scene_one: match
-match = ___
+match = re.search(r"coconuts", scene_one)
 
 # Print the start and end indexes of match
-print(__, __)
+if match:
+    print("inicial:", match.start())
+    print("final:", match.end())
+else:
+    print("Palavra 'coconuts' não encontrada.")
+
 print (match)
 
 # Write a regular expression to search for anything in square brackets: pattern1
-pattern1 = r"___"
+pattern1 = r"\[.*?\]"
 
 # Use re.search to find the first text in square brackets
-print(____)
-
+match = re.search(pattern1, scene_one)
+if match:
+    print("Texto entre colchetes:", match.group())
+else:
+    print("Nenhum texto encontrado.")
 # Find the script notation at the beginning of the fourth sentence and print it
-pattern2 = r"___"
-print(____)
+pattern2 = r"^\w+:"
+fourth_sentence = sentences[3]
+match = re.match(pattern2, fourth_sentence)
+if match:
+    print("Notação na quarta sentença:", match.group())
+else:
+    print("Notação não encontrada na quarta sentença.")
