4e28d7a835056a675a624d9a05ae4e279d92286f
lauramendescd <lauramendescd@gmail.com>
2023-09-24 13:03:24 -0300

    atualização

--- unidade2-NLP/1_regular_expressions/code3.py
+++ unidade2-NLP/1_regular_expressions/code3.py
@@ -1,8 +1,10 @@
 from nltk.tokenize import sent_tokenize
 from src.utils import get_sample_Santo_Graal
 import re
+
 scene_one = get_sample_Santo_Graal()
 sentences = sent_tokenize(scene_one)
+
 # Search for the first occurrence of "coconuts" in scene_one: match
 match = re.search(r"coconuts", scene_one)
 
@@ -11,21 +13,21 @@ if match:
     print("inicial:", match.start())
     print("final:", match.end())
 else:
-    print("Palavra 'coconuts' não encontrada.")
+    print("'coconuts' não foi encontrada.")
 
 print(match)
 
 # Write a regular expression to search for anything in square brackets: pattern1
-pattern1 = r"\[.*?\]"
+pattern1 = r"\[.*\]"
 
 # Use re.search to find the first text in square brackets
 match = re.search(pattern1, scene_one)
 if match:
     print("Texto entre colchetes:", match.group())
 else:
-    print("Nenhum texto encontrado.")
+    print("texto encontrado.")
 # Find the script notation at the beginning of the fourth sentence and print it
-pattern2 = r"^\w+:"
+pattern2 = r"[\w\s]+:"
 fourth_sentence = sentences[3]
 match = re.match(pattern2, fourth_sentence)
 if match:
--- unidade2-NLP/1_regular_expressions/code5.py
+++ unidade2-NLP/1_regular_expressions/code5.py
@@ -5,20 +5,22 @@ from src.utils import get_sample_Santo_Graal
 
 # Split the script into lines: lines
 holy_grail = get_sample_Santo_Graal()
-lines = holy_grail.___(___)
+lines = holy_grail.split('\n')
 
 # Replace all script lines for speaker
-pattern = "[A-Z]{2,}(\s)?(#\d)?([A-Z]{2,})?:"
-lines = [re.__(pattern, __, __) for l in __]
+pattern = "([A-Z]{2,}(?:\s#\d+)?:)"
+lines = [re.sub(pattern, '', line).strip() for line in lines]
 
 # Tokenize each line: tokenized_lines
-tokenized_lines = [regexp_tokenize(__,__) for s in __]
+tokenized_lines = [regexp_tokenize(line, pattern="\w+") for line in lines]
 
 # Make a frequency list of lengths: line_num_words
-line_num_words = [___ for t_line in tokenized_lines]
+line_num_words = [len(t_line) for t_line in tokenized_lines]
 
 # Plot a histogram of the line lengths
-___
+plt.hist(line_num_words, bins=20)
+plt.xlabel('Quantidade de palavras em uma linha')
+plt.ylabel('Frequência')
+plt.title('Distribuição dos Comprimentosem "Monty Python e o Santo Graal"')
+plt.show()
 
-# Show the plot
-___
