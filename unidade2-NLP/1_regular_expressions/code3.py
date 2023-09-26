7b87ed1bd4895c16a31c797eeb9bf3c9bf5d71b3
mateus <mateussilvamatos@alu.ufc.br>
2023-09-26 12:44:01 -0300

    Merge branch 'main' of https://github.com/alysonbz/data_minning_ufc into mateus_silva
    # Conflicts:
    #	unidade1/cluster_analisys.py

--- unidade2-NLP/1_regular_expressions/code3.py
+++ unidade2-NLP/1_regular_expressions/code3.py
@@ -1,21 +1,24 @@
 from nltk.tokenize import sent_tokenize
 from src.utils import get_sample_Santo_Graal
+import re
 
 scene_one =  get_sample_Santo_Graal()
 sentences =  sent_tokenize(scene_one)
 # Search for the first occurrence of "coconuts" in scene_one: match
-match = ___
-
+match = re.search('coconuts',scene_one)
 # Print the start and end indexes of match
-print(__, __)
+print(match.start(), match.end())
 print (match)
 
 # Write a regular expression to search for anything in square brackets: pattern1
-pattern1 = r"___"
+pattern1 = r"\[([^\]]+)\]"
 
 # Use re.search to find the first text in square brackets
-print(____)
+print(re.search(pattern1,scene_one))
 
 # Find the script notation at the beginning of the fourth sentence and print it
-pattern2 = r"___"
-print(____)
+pattern2 = r"^\s*([A-Z\s]+):"
+fourth_sentence = sentences[3]
+script_match = re.search(pattern2, fourth_sentence)
+script_notation = script_match.group(1) if script_match else None
+print(script_notation)
