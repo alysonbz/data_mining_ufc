5a16af267475fad35ad3aeb69feeb622e69feb46
mateus <mateussilvamatos@alu.ufc.br>
2023-10-03 20:42:50 -0300

    Merge branch 'main' of https://github.com/alysonbz/data_minning_ufc into mateus_silva
    # Conflicts:
    #	unidade1/cluster_analisys.py

--- unidade2-NLP/1_regular_expressions/code4.py
+++ unidade2-NLP/1_regular_expressions/code4.py
@@ -1,23 +1,28 @@
 # Import the necessary modules
 
-___
+# Import the necessary library
 from src.utils import get_tweets_sample
+from nltk.tokenize import TweetTokenizer  # Make sure you have NLTK installed
 
+# Get a sample of tweets
 tweets = get_tweets_sample()
 
 # Define a regex pattern to find hashtags: pattern1
+import re
 pattern1 = r"#\w+"
+
 # Use the pattern on the first tweet in the tweets list
-hashtags = ___
-print(__)
+hashtags = re.findall(pattern1, tweets[0])
+print(hashtags)
 
 # Write a pattern that matches both mentions (@) and hashtags
-pattern2 = r"(____)"
+pattern2 = r"[@#]\w+"
+
 # Use the pattern on the last tweet in the tweets list
-mentions_hashtags = ____
-print(___)
+mentions_hashtags = re.findall(pattern2, tweets[-1])
+print(mentions_hashtags)
 
 # Use the TweetTokenizer to tokenize all tweets into one list
-tknzr = ___
-all_tokens = [__ for t in __]
-print(___)
+tknzr = TweetTokenizer()
+all_tokens = [tknzr.tokenize(tweet) for tweet in tweets]
+print(all_tokens)
