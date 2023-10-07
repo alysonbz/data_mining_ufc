b9f691d797c6e9bd01a3736b14b1c9bf98067eff
Thays Ferreira <Thayzsferreiraua@gmail.com>
2023-10-07 18:13:37 -0300

    exercicios

--- unidade2-NLP/2_preprocessing/code4.py
+++ unidade2-NLP/2_preprocessing/code4.py
@@ -1,7 +1,8 @@
 import itertools
 from collections import defaultdict
 from gensim.corpora.dictionary import Dictionary
-from NLP.src.nlp_utils import get_pre_process_wiki_articles
+from src.utils import get_pre_process_wiki_articles
+
 
 # Create a Dictionary from the articles: dictionary
 articles = get_pre_process_wiki_articles()
@@ -18,21 +19,21 @@ bow_doc = sorted(doc, key=lambda w: w[1], reverse=True)
 
 # Print the top 5 words of the document alongside the count
 for word_id, word_count in bow_doc[:5]:
-    print("The token ",__,"appears ",__ , "times")
+    print("The token ", dictionary[word_id], "appears ", word_count, "times")
 
 # Create the defaultdict: total_word_count
 total_word_count = defaultdict(int)
 for word_id, word_count in itertools.chain.from_iterable(corpus):
-    __[__] += __
+    total_word_count[dictionary[word_id]] += word_count
 
 # Choose a key between 0 and 10 and show the count with a print function.
-key = __
-print("the key", __,"in defaultdict has count: ", ___,'\n')
+key = 2
+print("the key", key,"in defaultdict has count: ", total_word_count[dictionary[key]],'\n')
 
 # Create a sorted list from the defaultdict: sorted_word_count
-sorted_word_count = sorted(____, key=lambda w: w[1], reverse=True)
+sorted_word_count = sorted(total_word_count.items(), key=lambda w: w[1], reverse=True)
 
 # Print the top 5 words across all documents alongside the count
-for ____, ____ in sorted_word_count[:5]:
-    print(____.____(____), ____)
+for word, count in sorted_word_count[:5]:
+    print(word, count)
 
