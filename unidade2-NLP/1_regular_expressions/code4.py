680505e1c3d49bf11796c1c1597e38aab542ce15
Mateus Silva <104784855+MateusSilva12@users.noreply.github.com>
2023-11-02 16:51:06 -0300

    Update code4.py

--- unidade2-NLP/1_regular_expressions/code4.py
+++ unidade2-NLP/1_regular_expressions/code4.py
@@ -1,23 +1,23 @@
 # Import the necessary modules
-
-___
+import re
 from src.utils import get_tweets_sample
+from nltk.tokenize import TweetTokenizer
 
 tweets = get_tweets_sample()
 
 # Define a regex pattern to find hashtags: pattern1
 pattern1 = r"#\w+"
 # Use the pattern on the first tweet in the tweets list
-hashtags = ___
-print(__)
+hashtags = re.findall(pattern1, tweets[0])
+print("Hashtags:", hashtags)
 
 # Write a pattern that matches both mentions (@) and hashtags
-pattern2 = r"(____)"
+pattern2 = r"(@\w+|#\w+)"
 # Use the pattern on the last tweet in the tweets list
-mentions_hashtags = ____
-print(___)
+mentions_hashtags = re.findall(pattern2, tweets[-1])
+print("Mentions and Hashtags:", mentions_hashtags)
 
 # Use the TweetTokenizer to tokenize all tweets into one list
-tknzr = ___
-all_tokens = [__ for t in __]
-print(___)
+tknzr = TweetTokenizer()
+all_tokens = [tknzr.tokenize(tweet) for tweet in tweets]
+print("All Tokens:", all_tokens)
