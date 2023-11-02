7cccd795349c693070599f13a9e08395b496346e
Mateus Silva <104784855+MateusSilva12@users.noreply.github.com>
2023-11-02 16:35:57 -0300

    Update 1_dendrogram.py

--- unidade1-clusters/2_clustering/1_dendrogram.py
+++ unidade1-clusters/2_clustering/1_dendrogram.py
@@ -7,11 +7,18 @@ from scipy.cluster.hierarchy import linkage
 
 df = load_comic_con_dataset()
 
-# Calcula a matriz de distância
-distance_matrix = linkage(df[['x', 'y']])
+print(df.columns)
 
-# Crie um dendrograma
+
+import matplotlib.pyplot as plt
+from scipy.cluster.hierarchy import dendrogram, linkage
+
+# Carregue o DataFrame e selecione as colunas 'x_scaled' e 'y_scaled'
+distance_matrix = linkage(df[['x_scaled', 'y_scaled']])
+
+# Crie o dendrograma
 dn = dendrogram(distance_matrix)
 
 # Exiba o dendrograma
 plt.show()
+
