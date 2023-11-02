89ccf3ad084fd0a679d243ac7b62b0ef95a0381b
Mateus Silva <104784855+MateusSilva12@users.noreply.github.com>
2023-11-02 16:39:56 -0300

    Update 2_elbow_method.py

--- unidade1-clusters/2_clustering/2_elbow_method.py
+++ unidade1-clusters/2_clustering/2_elbow_method.py
@@ -1,25 +1,23 @@
 import matplotlib.pyplot as plt
 import seaborn as sns
-#Import kmeans from scipy
----
-
+from sklearn.cluster import KMeans
 from src.utils import load_comic_con_dataset
-
+import pandas as pd
 comic_con = load_comic_con_dataset()
 
 distortions = []
 num_clusters = range(1, 7)
 
 # Create a list of distortions from the kmeans function
-for i in ____:
-    cluster_centers, distortion = ____
-# append distortion on list distortions
-    ___
+for i in num_clusters:
+    kmeans = KMeans(n_clusters=i)
+    kmeans.fit(comic_con[['x_scaled', 'y_scaled']])
+    distortions.append(kmeans.inertia_)
 
 # Create a DataFrame with two lists - num_clusters, distortions
-elbow_plot = ___({'num_clusters': ____, 'distortions': ____})
+elbow_plot = pd.DataFrame({'num_clusters': num_clusters, 'distortions': distortions})
 
-# Creat a line plot of num_clusters and distortions
-sns.lineplot(x=____, y=____, data = ____)
+# Create a line plot of num_clusters and distortions
+sns.lineplot(x='num_clusters', y='distortions', data=elbow_plot)
 plt.xticks(num_clusters)
 plt.show()
