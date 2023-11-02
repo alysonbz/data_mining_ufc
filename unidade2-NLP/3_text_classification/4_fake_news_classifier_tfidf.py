1495d5ee8bc38b6863a92390c53aa7afd83c64e0
Mateus Silva <104784855+MateusSilva12@users.noreply.github.com>
2023-11-02 17:03:47 -0300

    Update 4_fake_news_classifier_tfidf.py

--- unidade2-NLP/3_text_classification/4_fake_news_classifier_tfidf.py
+++ unidade2-NLP/3_text_classification/4_fake_news_classifier_tfidf.py
@@ -1,25 +1,28 @@
-from src.utils import get_tfidf_fake_newes_dataset
+from src.utils import get_tfidf_fake_news_dataset
 import pandas as pd
 import matplotlib.pyplot as plt
-# Import the necessary modules
-from sklearn import  metrics
+from sklearn import metrics
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
 
-tfidf_train, tfidf_test , tfidf_vectorizer,tfidf_y_train, tfidf_y_test = get_tfidf_fake_newes_dataset()
+# Load the TF-IDF vectorizer and the training/testing data
+tfidf_train, tfidf_test, tfidf_vectorizer, tfidf_y_train, tfidf_y_test = get_tfidf_fake_news_dataset()
 
 # Instantiate a Multinomial Naive Bayes classifier: nb_classifier
-nb_classifier = ___
+nb_classifier = MultinomialNB()
 
 # Fit the classifier to the training data
-___
+nb_classifier.fit(tfidf_train, tfidf_y_train)
 
 # Create the predicted tags: pred
-pred = ___
+pred = nb_classifier.predict(tfidf_test)
 
 # Calculate the accuracy score: score
-score = __
-print(score)
+score = metrics.accuracy_score(tfidf_y_test, pred)
+print("Accuracy Score:", score)
 
 # Calculate the confusion matrix: cm
-__
+cm = confusion_matrix(tfidf_y_test, pred)
+disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=['FAKE', 'REAL'])
+disp.plot()
+plt.show()
