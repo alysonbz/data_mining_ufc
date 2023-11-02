0040cbb7c8e65e30b4a1963d175c0da0f5741a6e
Mateus Silva <104784855+MateusSilva12@users.noreply.github.com>
2023-11-02 17:08:52 -0300

    Update test.py

--- test.py
+++ test.py
@@ -1,18 +1,28 @@
-import re
-'''resp = re.match('mateus','mateus eh lindo')
-print(resp)
-resp1 = re.match('feio','mateus eh feio')
-print(resp1)
-word_regex = '\s+'
-resp = re.split(word_regex,'semana de aula')
-print(resp)
-word_regex = r'[a-z]\w+'
-resp = re.split(word_regex,'4Semana Quente! De Aula')
-print(resp)'''
-import re
-pattern1 = '(\w+|\?|!)'
-pattern2 = '(\w+|#\d+|\?\!)'
-pattern3 = '(#\d\w+\?!)'
-pattern4 = '\s+'
-print(re.findall(pattern1,"SOLDIER #1: Found them? In mercea? The coconut's tropical"))
-from nltk.tokenize import sent_tokenize
+from src.utils import get_tfidf_fake_news_dataset
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn import metrics
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
+
+# Load the TF-IDF vectorizer and the training/testing data
+tfidf_train, tfidf_test, tfidf_vectorizer, tfidf_y_train, tfidf_y_test = get_tfidf_fake_news_dataset()
+
+# Instantiate a Multinomial Naive Bayes classifier: nb_classifier
+nb_classifier = MultinomialNB()
+
+# Fit the classifier to the training data
+nb_classifier.fit(tfidf_train, tfidf_y_train)
+
+# Create the predicted tags: pred
+pred = nb_classifier.predict(tfidf_test)
+
+# Calculate the accuracy score: score
+score = metrics.accuracy_score(tfidf_y_test, pred)
+print("Accuracy Score:", score)
+
+# Calculate the confusion matrix: cm
+cm = confusion_matrix(tfidf_y_test, pred)
+disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=['FAKE', 'REAL'])
+disp.plot()
+plt.show()
