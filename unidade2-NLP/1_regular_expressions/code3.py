b8ba0d3da1ff346cc2d1ec32e487e3b0f697af8b
mateus <mateussilvamatos@alu.ufc.br>
2023-11-02 12:21:16 -0300

    Revert "Merge branch 'main' of https://github.com/alysonbz/data_minning_ufc into mateus_silva"
    This reverts commit daeb3064400488ac7269bea6b040633a10861aba.

--- unidade2-NLP/1_regular_expressions/code3.py
+++ unidade2-NLP/1_regular_expressions/code3.py
@@ -11,13 +11,14 @@ print(match.start(), match.end())
 print (match)
 
 # Write a regular expression to search for anything in square brackets: pattern1
-pattern1 = r"\[.*]"
+pattern1 = r"\[([^\]]+)\]"
 
 # Use re.search to find the first text in square brackets
 print(re.search(pattern1,scene_one))
 
 # Find the script notation at the beginning of the fourth sentence and print it
 pattern2 = r"^\s*([A-Z\s]+):"
-
-print(re.match(pattern2,sentences[3]))
-
+fourth_sentence = sentences[3]
+script_match = re.search(pattern2, fourth_sentence)
+script_notation = script_match.group(1) if script_match else None
+print(script_notation)
