1dbed6a7790e51c6b58b2a663eb2966cdfb92033
alysonribeiro <alysonribeiro@mobitbrasil.com.br>
2023-11-07 15:10:19 -0300

    Unidade3-PDI

--- Unidade3-PDI/1 - pre-processamento/code1.py
+++ Unidade3-PDI/1 - pre-processamento/code1.py
@@ -1,16 +1,15 @@
 from src.pdi_utils import show_image
 # Import the modules from skimage
-from skimage import data, color
+from skimage import ____, ____
 
 # Load the rocket image
-rocket = data.rocket()
+rocket = data.____()
 
 # Convert the image to grayscale
-gray_scaled_rocket = color.rgb2gray(rocket)
+gray_scaled_rocket = color.____(____)
 
 # Show the original image
 show_image(rocket, 'Original RGB image')
 
 # Show the grayscale image
 show_image(gray_scaled_rocket, 'Grayscale image')
-#Mateus
