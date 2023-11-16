4bf79f7d639e25ac6ae99e2c7b17e344d240d801
mateus <mateussilvamatos@alu.ufc.br>
2023-11-16 16:27:28 -0300

    ATIVIDADES

--- Unidade3-PDI/1 - pre-processamento/code3.py
+++ Unidade3-PDI/1 - pre-processamento/code3.py
@@ -1,23 +1,21 @@
-from src.pdi_utils import load_lena
+from src.pdi_utils import load_red_roses, show_image
 import matplotlib.pyplot as plt
 
-from src.pdi_utils import load_red_roses,show_image
-import matplotlib.pyplot as plt
-
+# Load the red_roses image
 image = load_red_roses()
 
-# Show original image
-__(__,'image RGB')
+# Show original RGB image
+show_image(image, 'Image RGB')
 
 # Obtain the red channel
-red_channel = __
+red_channel = image[:, :, 0]  # Assuming the red channel is the first channel (index 0)
 
-# Show original image
-__(__,'image red channel')
+# Show the red channel image
+show_image(red_channel, 'Image Red Channel')
 
 # Plot the red histogram with bins in a range of 256
-plt.__(__, bins=__)
+plt.hist(red_channel.flatten(), bins=256, color='red', alpha=0.7)
 
-# Set title and show
+# Set title and show the histogram
 plt.title('Red Histogram')
 plt.show()
