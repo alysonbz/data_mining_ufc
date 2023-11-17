c755df4202504f8a22439191e831c30197fe0f5e
João Barbosa <davibarbosabdj@alu.ufc.br>
2023-11-17 13:24:22 -0300

    add_modificacoes

--- Unidade3-PDI/1 - pre-processamento/code1.py
+++ Unidade3-PDI/1 - pre-processamento/code1.py
@@ -13,24 +13,3 @@ show_image(rocket, 'Original RGB image')
 
 # Show the grayscale image
 show_image(gray_scaled_rocket, 'Grayscale image')
-
-
-import cv2
-import numpy as np
-
-# Carregar a imagem
-imagem = cv2.imread("C:\\Users\\joaod\\Downloads\\53150885441_5c4f6332f1_o.jpg", cv2.IMREAD_GRAYSCALE)
-
-# Converter a imagem em uma matriz NumPy
-matriz_pdi = np.array(imagem)
-
-# Agora, 'matriz_pdi' contém os valores dos pixels da imagem
-
-# Para exibir a matriz
-print(matriz_pdi)
-
-import matplotlib.pyplot as plt
-
-# Exibir a imagem
-plt.imshow(matriz_pdi, cmap='gray')
-plt.show()
