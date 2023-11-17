62f37db37f8af82bdc36ae90e6367bf9e2c3a6bf
João Barbosa <davibarbosabdj@alu.ufc.br>
2023-11-17 13:35:00 -0300

    add_modificacoes

--- Unidade3-PDI/1 - pre-processamento/code3.py
+++ Unidade3-PDI/1 - pre-processamento/code3.py
@@ -16,7 +16,7 @@ red_channel = image[:, :, 0]
 show_image(image,'image red channel')
 
 # Plot the red histogram with bins in a range of 256
-plt.hist(red_channel.ravel(), bins=256, color='red')
+plt.hist(red_channel.ravel(), bins=255, color='red')
 
 # Set title and show
 plt.title('Red Histogram')
