6aa48a215b05298b576b2762ed74de644b45260e
alysonribeiro <alysonribeiro@mobitbrasil.com.br>
2023-11-17 08:51:10 -0300

    Unidade3-PDI

---
+++ Unidade3-PDI/2-atributes_extraction/code3.py
@@ -0,0 +1,26 @@
+
+# Import Gaussian filter
+from skimage.filters import gaussian
+from src.pdi_utils import load_building_image,show_image
+building_image = load_building_image()
+
+
+# Apply filter sigma = 1
+gaussian_image = gaussian(___, sigma=___,multichannel = True)
+
+# Show original and resulting image to compare
+show_image(building_image, "Original")
+show_image(gaussian_image, "Reduced sharpness Gaussian with sigma = 1")
+
+
+# Apply gaussian filter sigma = 5
+____
+
+# Show resulting image to compare
+show_image(___, "Reduced sharpness Gaussian with sigma = 5")
+
+# Apply filter sigma = 10
+____
+
+# Show resulting image to compare
+show_image(___, "Reduced sharpness Gaussian with sigma = 10")
---
+++ Unidade3-PDI/2-atributes_extraction/code6.py
@@ -0,0 +1,2 @@
+from src.pdi_utils import load_soaps_image
+
