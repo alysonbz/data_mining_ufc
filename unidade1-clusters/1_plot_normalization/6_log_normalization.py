7ec1313be6d5726e20138c69595f1eccd51e6ebc
DSsavioluis <luissavio150@gmail.com>
2024-03-31 22:03:50 -0300

    atualização das atividades

--- unidade1-clusters/1_plot_normalization/6_log_normalization.py
+++ unidade1-clusters/1_plot_normalization/6_log_normalization.py
@@ -1,19 +1,20 @@
 import numpy as np
 from src.utils import load_wine_dataset
 import pandas as pd
+from statistics import variance
 
 wine = load_wine_dataset()
 
 pd.set_option('display.max_columns', None)
 
 #print as caractéristicas estatísticas do dataset wine
-print(wine.__)
+#print(wine.describe())
 
 ## Aplique a função de nomarlização logarítmica na coluna Proline
-wine[__] = np.__(___)
+wine['Proline_log'] = np.log(wine['Proline'])
 #
 # Print a variância da coluna proline
-print(___)
+print(variance(wine['Proline']))
 
 # print a variância da coluna proline normalizada
-print(___)
+print(variance(wine['Proline_log']))
