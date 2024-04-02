44c8c01e8455663d6e2fda54400001705ea0e3d0
DSsavioluis <luissavio150@gmail.com>
2024-04-02 15:53:43 -0300

    atualização das atividades_2

--- unidade1-clusters/2_clustering/2_elbow_method.py
+++ unidade1-clusters/2_clustering/2_elbow_method.py
@@ -1,7 +1,8 @@
 import matplotlib.pyplot as plt
+import pandas as pd
 import seaborn as sns
 #Import kmeans from scipy
----
+from scipy.cluster.vq import kmeans
 
 from src.utils import load_comic_con_dataset
 
@@ -11,15 +12,15 @@ distortions = []
 num_clusters = range(1, 7)
 
 # Create a list of distortions from the kmeans function
-for i in ____:
-    cluster_centers, distortion = ____
+for i in num_clusters:
+    cluster_centers, distortion = kmeans(comic_con[['x_scaled', 'y_scaled']],i)
 # append distortion on list distortions
-    ___
+    distortions.append(distortion)
 
 # Create a DataFrame with two lists - num_clusters, distortions
-elbow_plot = ___({'num_clusters': ____, 'distortions': ____})
+elbow_plot = pd.DataFrame({'num_clusters': num_clusters, 'distortions': distortions})
 
 # Creat a line plot of num_clusters and distortions
-sns.lineplot(x=____, y=____, data = ____)
+sns.lineplot(x='num_clusters', y='distortions', data = elbow_plot)
 plt.xticks(num_clusters)
 plt.show()
--- unidade1-clusters/2_clustering/3_limitations_kmeans.py
+++ unidade1-clusters/2_clustering/3_limitations_kmeans.py
@@ -7,14 +7,14 @@ from src.utils import load_fifa_dataset
 
 fifa = load_fifa_dataset()
 # Fit the data into a k-means algorithm
-cluster_centers,_ =___
+cluster_centers,_ = kmeans(fifa[['sliding_tackle', 'aggression']], 3)
 
 # Assign cluster labels
-fifa['cluster_labels'], _ =___
+fifa['cluster_labels'], _ = vq(fifa[['sliding_tackle', 'aggression']], cluster_centers)
 
 # Display cluster centers
-print(fifa[['sliding_tackle','aggression', 'cluster_labels']].groupby(__).__())
+print(fifa[['sliding_tackle','aggression', 'cluster_labels']].groupby('cluster_labels').mean())
 
 # Create a scatter plot through seaborn
-___
+sns.scatterplot(data=fifa, x='sliding_tackle', y='aggression', hue='cluster_labels')
 plt.show()
