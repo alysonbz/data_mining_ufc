35ac9b1106814d24c40b0c2ad52463043b618597
erick_9ge6pj1 <erickcoutinho@alu.ufc.br>
2024-04-09 12:10:33 -0300

    Atividades - atualização

--- unidade1-clusters/1_plot_normalization/2_1_clusters_similarity.py
+++ unidade1-clusters/1_plot_normalization/2_1_clusters_similarity.py
@@ -1,21 +1,44 @@
-from src.utils import load_pokemon_dataset
-import numpy as np
+import math
 
+def euclidean_distance(point1, point2):
+    return math.sqrt(sum((x - y) ** 2 for x, y in zip(point1, point2)))
 
 def compute_single_linkage(cluster1,cluster2):
-     return None
+
+    min_distance = float('inf')
+    for p1 in cluster1:
+        for p2 in cluster2:
+            distance = euclidean_distance(p1, p2)
+            if distance < min_distance:
+                min_distance = distance
+    return min_distance
 
 def compute_complete_linkage(cluster1, cluster2):
-     return None
+    max_distance = 0
+    for p1 in cluster1:
+        for p2 in cluster2:
+            distance = euclidean_distance(p1, p2)
+            if distance > max_distance:
+                max_distance = distance
+    return max_distance
+
 
 def compute_average_linkage(cluster1, cluster2):
-     return None
+    total_distance = sum(euclidean_distance(p1, p2) for p1 in cluster1 for p2 in cluster2)
+    return total_distance / (len(cluster1) * len(cluster2))
 
 def compute_centroid_linkage(cluster1,cluster2):
-    return None
+    centroid1 = [sum(coord) / len(cluster1) for coord in zip(*cluster1)]
+    centroid2 = [sum(coord) / len(cluster2) for coord in zip(*cluster2)]
+    return euclidean_distance(centroid1, centroid2)
 
 def compute_ward_linkage(cluster1,cluster2):
-    return None
+    centroid1 = [sum(coord) / len(cluster1) for coord in zip(*cluster1)]
+    centroid2 = [sum(coord) / len(cluster2) for coord in zip(*cluster2)]
+    merged_cluster = cluster1 + cluster2
+    merged_centroid = [sum(coord) / len(merged_cluster) for coord in zip(*merged_cluster)]
+    return (len(cluster1) * euclidean_distance(centroid1, merged_centroid) +
+            len(cluster2) * euclidean_distance(centroid2, merged_centroid)) / len(merged_cluster)
 
 
 cluster1 = [[9.0,8.0],[6.0,4.0],[2.0,10.0],[3.0,6.0],[1.0,0.0]]
--- unidade1-clusters/1_plot_normalization/3_1_manual_kmeans.py
+++ unidade1-clusters/1_plot_normalization/3_1_manual_kmeans.py
@@ -1,37 +1,67 @@
 from src.utils import load_pokemon_dataset
-
-
-df = load_pokemon_dataset()
-
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+import pandas as pd
 
 def set_random_cluster_coordinate(num_of_cluster):
     coord_list = []
-    label_list = range(0,num_of_cluster)
-    #preecher a lista com quatro coordenadas aleatótias.
-    return coord_list , label_list
+    label_list = range(0, num_of_cluster)
+    for _ in range(num_of_cluster):
+        x = np.random.randint(0, 150)
+        y = np.random.randint(0, 150)
+        coord_list.append([x, y])
+    return coord_list, label_list
 
-def create_points(df):
-    coords = [] # [ [x1,y1], [x2,y2] , [x3,y3]....]
-    return coords
 
-def dist_euclidian(p1,p2):
-    dist = 0
-    return dist
+def dist_euclidian(p1, p2):
+    return np.linalg.norm(np.array(p1) - np.array(p2))
 
-def kmeans(df,num_of_cluster):
 
-    centroids ,  centroids_labels = set_random_cluster_coordinate(num_of_cluster)
+def kmeans(data, num_of_cluster):
+    centroids, centroids_labels = set_random_cluster_coordinate(num_of_cluster)
     increase_cluster = True
-    coords  = create_points(df)
-    coord_label = []  #
-    while increase_cluster == True:
-        for coord in coords:
-           coord_label.append(None)
-            ##algoritmo
+    coord_labels = [None] * len(data[0])
 
-    return  coord_label
+    while increase_cluster:
+        increase_cluster = False
+        for i, (x, y) in enumerate(zip(data[0], data[1])):
+            min_dist = float('inf')
+            for j, (centroid_x, centroid_y) in enumerate(centroids):
+                dist = dist_euclidian((x, y), (centroid_x, centroid_y))
+                if dist < min_dist:
+                    min_dist = dist
+                    coord_labels[i] = centroids_labels[j]
+        new_centroids = []
+        new_centroids_labels = []
+        for label in set(coord_labels):
+            cluster_coords = [(x, y) for (x, y), l in zip(zip(data[0], data[1]), coord_labels) if l == label]
+            if len(cluster_coords) == 0:
+                continue
+            centroid = np.mean(cluster_coords, axis=0)
+            new_centroids.append(centroid)
+            new_centroids_labels.append(label)
+        if len(new_centroids) != len(centroids):
+            increase_cluster = True
+        centroids = new_centroids
+        centroids_labels = new_centroids_labels
+
+    return coord_labels
 
 
+# Carregar o conjunto de dados
+data = load_pokemon_dataset()
 
-
+# Executar KMeans com 2 clusters
 num_of_clusters = 2
+cluster_labels = kmeans(data, num_of_clusters)
+
+# Criar um DataFrame do pandas para os dados
+df = pd.DataFrame({'Attack': data[0], 'Defense': data[1], 'cluster_labels': cluster_labels})
+
+# Plotar os dados
+sns.scatterplot(data=df, x='Attack', y='Defense', hue='cluster_labels')
+plt.title('KMeans Clustering')
+plt.xlabel('Attack')
+plt.ylabel('Defense')
+plt.show()
