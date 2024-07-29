eff2da34c82d4a028004338035e40fd8de014ea3
MASTER <aryaanialekyni@gmail.com>
2024-07-28 22:23:56 -0300

    Atividades

--- unidade2-NLP/1_regular_expressions/Atividade_regular_expression.py
+++ unidade2-NLP/1_regular_expressions/Atividade_regular_expression.py
@@ -1,11 +1,11 @@
 import re
 
 #1. **Contagem de Correspondências:**
-#   Escreva um programa que conte quantas vezes a palavra "Python" aparece em uma determinada string usando expressões regulares.
-
+#Escreva um programa que conte quantas vezes a palavra "Python" aparece em uma determinada string usando expressões regulares.
+print('Primeira atividade')
 def cont_word(text):
-    regex = r"\bPython\b"
-    resp = re.findall(regex,text, re.IGNORECASE)
+    regex = "Python"
+    resp = re.findall(regex,text)
     return len(resp)
 text = "A liguagem de programação em Python é a mais utilizada do mundo," \
        " fazendo de Python também a principal ferramenta para pesquisa de ML. " \
@@ -14,5 +14,88 @@ text = "A liguagem de programação em Python é a mais utilizada do mundo," \
 print(cont_word(text))
 
 
+# Validação de email
+print('Segunda atividade')
+def valid_email(email):
+    regex = r'^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'
+    return bool(re.match(regex, email))
 
+text =  "alekyne@example.com"
+text2 = "alek_synth@example"
 
+print(valid_email(text2))
+
+# extração de números de telefone
+print('Terceira atividade')
+def extract_phone_numbers(text):
+    regex = r'\(?\d{2}\)?\s?\d{4,5}-\d{4}|\d{4}-\d{3}-\d{3}|\d{4}-\d{4}'
+    return re.findall(regex, text)
+
+text = "Meu telefone é (11) 98765-4321"
+text2 = "Meu telefone é 9876-4321"
+print(extract_phone_numbers(text))
+
+# Substituição de palavra
+print('Quarta atividade')
+def replace_word(text, old, new):
+    return re.sub(old, new, text)
+text = "Olá, meu nome é Alekyne. E estou passeando com meu gato."
+print(replace_word(text, "gato", "cachorro"))
+
+# Extração de URL
+
+print('Quinta atividade')
+
+def extract_urls(text):
+    regex = r'(https?://[^\s]+)'
+    return re.findall(regex, text)
+
+text = "Acesse o site https://www.O_melhor_de_todos.com e veja as novidades em https://www.Alekyne10.com"
+print(extract_urls(text))
+
+# Verificação de senha segura
+print('Sexta atividade')
+def is_secure_password(password):
+    regex = r'^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$'
+    return bool(re.match(regex, password))
+
+text = "Alekyne10"
+text2 = 'Alekyne10@'
+print(is_secure_password(text))
+
+# extração de palavra
+print('Sétima atividade')
+def extract_words(text):
+    regex = r'\w+'
+    return re.findall(regex, text)
+
+text = "Olá, meu nome é Alekyne. E estou passeando com meu gato."
+print(extract_words(text))
+
+# Validação de data
+print('Oitava atividade')
+def valid_date(date):
+    regex = r'^\d{4}-\d{2}-\d{2}$'
+    return bool(re.match(regex, date))
+
+text = "2022-12-31"
+text2 = "202-122-31"
+print(valid_date(text))
+
+# Extração de nomes proprios
+print('Nona atividade')
+def extract_names(text):
+    regex = r'[A-Z][a-z]+(?: [A-Z][a-z]+)*'
+    return re.findall(regex, text)
+
+text = "olá, meu nome é Alekyne. E estou passeando com a Livia."
+print(extract_names(text))
+
+# Contagem de vogais
+print('Décima atividade')
+def count_vowels(text):
+    regex = r'[aeiouAEIOU]'
+    return len(re.findall(regex, text))
+
+text = "UFC estava de greve."
+print(count_vowels(text))
--- unidade2-NLP/1_regular_expressions/code2.py
+++ unidade2-NLP/1_regular_expressions/code2.py
@@ -1,16 +1,17 @@
 # Import necessary modules
+import nltk
 from nltk.tokenize import word_tokenize , sent_tokenize
 from src.utils import get_sample_Santo_Graal
 
 # Split scene_one into sentences: sentences
 scene_one = get_sample_Santo_Graal()
-sentences =
+sentences = sent_tokenize(scene_one)
 
 # Use word_tokenize to tokenize the fourth sentence: tokenized_sent
-tokenized_sent = _____
+tokenized_sent = word_tokenize(sentences[3])
 
 # Make a set of unique tokens in the entire scene: unique_tokens
-unique_tokens = set(____)
+unique_tokens = set(tokenized_sent)
 
 # Print the unique tokens result
-print(___)
+print(unique_tokens)
