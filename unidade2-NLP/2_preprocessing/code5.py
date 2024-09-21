3b714655ae956037c40c23780832150aea35d04e
Luciana <lucianasmartins779@gmail.com>
2024-09-21 18:47:37 -0300

    .

--- unidade2-NLP/2_preprocessing/code5.py
+++ unidade2-NLP/2_preprocessing/code5.py
@@ -13,16 +13,17 @@ corpus = [dictionary.doc2bow(article) for article in articles]
 doc = corpus[4]
 
 # Create a new TfidfModel using the corpus: tfidf
-tfidf = __(__)
+tfidf = TfidfModel(corpus)
+
 # Calculate the tfidf weights of doc: tfidf_weights
-tfidf_weights = __[__]
+tfidf_weights = tfidf[doc]
 
 # Print the first five weights
-print(___)
+print(tfidf_weights[:5])
 
 # Sort the weights from highest to lowest: sorted_tfidf_weights
-sorted_tfidf_weights = ___
+sorted_tfidf_weights = sorted(tfidf_weights, key=lambda w: w[1], reverse=True)
 
 # Print the top 5 weighted words
 for term_id, weight in sorted_tfidf_weights[:5]:
-    print(__,__)
+    print(dictionary.get(term_id), weight)
